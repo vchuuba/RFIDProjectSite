165a383f13d97ff45b92bbf0a6e8eb3f4d3a0677
vchuuba <richardel.leang@dawsoncollege.qc.ca>
2026-03-15 21:27:32 -0400

    PHP Dump

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dawson Sports Equipment Renting Portal</title>
+    <link href="style.css" rel="stylesheet" type="text/css" media="all">
+    <script>
+    </script>
+  </head>
+  <body>
+    <p><?= var_dump($_SERVER) ?></p>
+  </body>
+</html>
