a0ec22b387d64f6091d13fe3f9a7cceed0bae761
vchuuba <richardel.leang@dawsoncollege.qc.ca>
2026-04-06 02:07:16 -0400

    fixing database files

--- get-post-esp-data.php
+++ get-post-esp-data.php
@@ -7,23 +7,14 @@
     // Your Database user password
     $password = "Shell111";
 
-    // Keep this API Key value to be compatible with the ESP32 code provided in the project page. 
-    // If you change this value, the ESP32 sketch needs to match
-    $api_key_value = "tPmAT5Ab3j7F9";
+    $keyword = $cardID = $id = "";
 
-    $api_key = "";
-    $cardID = "";
-    $id = "";
+        // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+    if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
 
-    if ($_SERVER["REQUEST_METHOD"] == "GET") {
-
-            // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-            // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-
+    if ($_SERVER["REQUEST_METHOD"] == "GET") { // return data to esp32
         $sql = "SELECT id, cardID FROM productList WHERE id=26";
         $result = $conn->query($sql);
         while ($row = $result->fetch_assoc()) {
@@ -34,30 +25,22 @@
     }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        $api_key = test_input($_POST["api_key"]);
-
-        if($api_key == $api_key_value) {
-
+        $keyword = test_input($_POST["keyword"]);
+        if($keyword == "tPmAT5Ab3j7F9") { // test
             $cardID = test_input($_POST["cardID"]);
-            
-            // Create connection
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            // Check connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            } 
-            
             $sql = "update productList set cardID = '" . $cardID . "' where id = 26";
-            
             if ($conn->query($sql) === TRUE) {
                 echo "Record updated successfully";
             } else {echo "Error: " . $sql . "<br>" . $conn->error;}
-        
             $conn->close();
-
-        } else {echo "Wrong API Key provided.";}
-
+        } else if ($keyword == "MFRC522POST") { // mfrc522 post
+            $cardID = test_input($_POST["cardID"]);
+            $sql = "update productList set cardID = '" . $cardID . "' where id = 26";
+            if ($conn->query($sql) === TRUE) {
+                echo "Record updated successfully";
+            } else {echo "Error: " . $sql . "<br>" . $conn->error;}
+            $conn->close();
+        } else {echo "Wrong Keyword provided.";}
     } else {echo "No data posted with HTTP POST.";}
 
     function test_input($data) {
