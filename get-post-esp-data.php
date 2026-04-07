cb1a6457af429f9f7e322ec1943991fe9240d8f6
vchuuba <richardel.leang@dawsoncollege.qc.ca>
2026-04-07 10:18:03 -0400

    still adding options to post and get

--- get-post-esp-data.php
+++ get-post-esp-data.php
@@ -7,7 +7,7 @@
     // Your Database user password
     $password = "Shell111";
 
-    $keyword = $cardID = $id = "";
+    // $keyword = $cardID = $id = "";
 
     //read body
     $raw = file_get_contents('php://input');
@@ -19,17 +19,27 @@
     if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
 
     if ($_SERVER["REQUEST_METHOD"] == "GET") { // return data to esp32
-        $sql = "SELECT id, cardID FROM productList WHERE id=26";
-        $result = $conn->query($sql);
-        while ($row = $result->fetch_assoc()) {
-            $rows[$row["id"]] = $row["cardID"];
-        }
-        echo json_encode($rows);
-        $conn->close();
-    }
+        if($data["keyword"] == "MFRC522GET") { // mfrc522 get
+            $sql = "SELECT id, tag FROM clientID WHERE id=1";
+            $result = $conn->query($sql);
+            while ($row = $result->fetch_assoc()) {
+                $rows[$row["id"]] = $row["tag"];
+            }
+            echo json_encode($rows);
+            $conn->close();
+        } else if ($data["keyword"] == "PN532GET") { // pn532 get
+            $sql = "SELECT id, cardID FROM productList WHERE id=6";
+            $result = $conn->query($sql);
+            while ($row = $result->fetch_assoc()) {
+                $rows[$row["id"]] = $row["cardID"];
+            }
+            echo json_encode($rows);
+            $conn->close();
+        } else {echo "Wrong Keyword provided.";} // error
+    } else {echo "No data gotten with HTTP GET.";}
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $keyword = test_input($data["keyword"]); // replace with $_POST["keyword"] for url encoding
+        // insert $keyword = test_input($_POST["keyword"]) for url encoding
         if($keyword == "tPmAT5Ab3j7F9") { // test with url encoding
             $cardID = test_input($_POST["cardID"]);
             $sql = "update clientID set tag = '" . $cardID . "' where id = 26";
@@ -39,6 +49,12 @@
             $conn->close();
         } else if ($data["keyword"] == "MFRC522POST") { // mfrc522 post with json encoding
             $sql = "update clientID set tag = '" . $data["cardID"] . "' where username = 'realtest'";
+            if ($conn->query($sql) === TRUE) {
+                echo "Record updated successfully";
+            } else {echo "Error: " . $sql . "<br>" . $conn->error;} 
+            $conn->close();
+        } else if ($data["keyword"] == "PN532POST") { // pn532 post with json encoding
+            $sql = "update productList set cardID = '" . $data["cardID"] . "' where productName = 'realtest'";
             if ($conn->query($sql) === TRUE) {
                 echo "Record updated successfully";
             } else {echo "Error: " . $sql . "<br>" . $conn->error;} 
