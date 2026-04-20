fd724c1aee57e35c6bdac54cca6331b7865b3b36
vchuuba <richardel.leang@dawsoncollege.qc.ca>
2026-04-20 01:22:40 -0400

    removing json object attempt. sticking to url encoding for now

--- get-post-esp-data.php
+++ get-post-esp-data.php
@@ -7,123 +7,106 @@
     // Your Database user password
     $password = "Shell111";
 
-
-    //read body
-    $raw = file_get_contents('php://input');
-    $data = json_decode($raw, true);
-
-    // empty vars
-    $default = array("keyword" => "none", "cardID" => "none");
-    $fail = ["keyword" => "none", "cardID" => "none"];
-
-    //     // Create connection
-    // $conn = new mysqli($servername, $username, $password, $dbname);
-    //     // Check connection
-    // if ($conn->connect_error)
-    // {
-    //     die("Connection failed: " . $conn->connect_error);
-    // }
+        // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+    if ($conn->connect_error)
+    {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
 
-    // if ($_SERVER["REQUEST_METHOD"] == "POST")
-    // {
-    //     if ($data["keyword"] == "MFRC522AUT")
-    //     { // client authentication
-    //         $sql = "SELECT id, cardID FROM clientID WHERE cardID = '" . $data["cardID"] . "'";
-    //         $result = $conn->query($sql);
-    //         if ($conn->query($result) === TRUE)
-    //         {
-    //             while ($row = $result->fetch_assoc()) {
-    //                 $rows[$row["id"]] = $row["cardID"];
-    //             }
-    //             if ($data["cardID"] == $row["cardID"])
-    //             {
-    //                 echo json_encode($rows);
-    //             }
-    //             else 
-    //             {
-    //                 echo json_encode($default);
-    //             }
-    //         }
-    //         $conn->close();
-    //     }
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        if (test_input($_POST["keyword"]) == "MFRC522AUT")
+        { // client authentication
+            $sql = "SELECT id, cardID FROM clientID WHERE cardID = '" . test_input($_POST["cardID"]) . "'";
+            $result = $conn->query($sql);
+            if ($conn->query($result) === TRUE)
+            {
+                echo "Authenticated";
+            }
+            else 
+            {
+                echo "Not authenticated";
+            }
+            $conn->close();
+        }
 
 
-    //     else if ($data["keyword"] == "MFRC522REG")
-    //     { // client registration
-    //         $sql = "SELECT id, cardID FROM clientID WHERE cardID = '" . $data["cardID"] . "'";
-    //         $result = $conn->query($sql);
-    //         if ($conn->query($result) === TRUE)
-    //         {
-    //             while ($row = $result->fetch_assoc()) {
-    //                 $rows[$row["id"]] = $row["cardID"];
-    //             }
-    //             if ($data["cardID"] == $row["cardID"])
-    //             {
-    //                 $sql = "INSERT INTO clientID (cardID) VALUES ('" . $data["cardID"] . "')";
-    //                 if ($conn->query($sql) === TRUE)
-    //                 {
-    //                     echo json_encode($rows);
-    //                 }
-    //             }
-    //             else 
-    //             {
-    //                 echo json_encode($default);
-    //             }
-    //         }
-    //         $conn->close();
-    //     }
+        else if (test_input($_POST["keyword"]) == "MFRC522REG")
+        { // client registration
+            $sql = "SELECT id, cardID FROM clientID WHERE cardID = '" . test_input($_POST["cardID"]) . "'";
+            $result = $conn->query($sql);
+            if ($conn->query($result) === TRUE)
+            {
+                $sql = "INSERT INTO clientID (cardID) VALUES ('" . test_input($_POST["cardID"]) . "')";
+                if ($conn->query($sql) === TRUE)
+                {
+                    echo "Registered";
+                }
+                else 
+                {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            }
+            else 
+            {
+                echo "Not registered";
+            }
+            $conn->close();
+        }
 
 
-    //     else if ($data["keyword"] == "PN532DET")
-    //     { // item detection
-    //         $sql = "SELECT id, cardID FROM productList WHERE cardID = '" . $data["cardID"] . "'";
-    //         $result = $conn->query($sql);
-    //         if ($conn->query($result) === TRUE)
-    //         {
-    //             while ($row = $result->fetch_assoc()) {
-    //                 $rows[$row["id"]] = $row["cardID"];
-    //             }
-    //             if ($data["cardID"] == $row["cardID"])
-    //             {
-    //                 echo json_encode($rows);
-    //             }
-    //             else 
-    //             {
-    //                 echo json_encode($default);
-    //             }
-    //         }
-    //         $conn->close();
-    //     }
+        else if (test_input($_POST["keyword"]) == "PN532DET")
+        { // item detection
+            $sql = "SELECT id, cardID FROM productList WHERE cardID = '" . test_input($_POST["cardID"]) . "'";
+            $result = $conn->query($sql);
+            if ($conn->query($result) === TRUE)
+            {
+                while ($row = $result->fetch_assoc()) {
+                    $rows[$row["id"]] = $row["cardID"];
+                }
+                if (test_input($_POST["cardID"]) == $row["cardID"])
+                {
+                    echo "Detected";
+                }
+                else 
+                {
+                    echo "Not detected";
+                }
+            }
+            $conn->close();
+        }
 
 
-    //     else if ($data["keyword"] == "PN532SEL")
-    //     { // client registration
-    //         $sql = "update productList set cardID = '" . $data["cardID"] . "' where productName = 'realtest'";
-    //         if ($conn->query($sql) === TRUE)
-    //         {
-    //             echo "Record updated successfully";
-    //         }
-    //         else {echo "Error: " . $sql . "<br>" . $conn->error;} 
-    //         $conn->close();
-    //     }
+        else if (test_input($_POST["keyword"]) == "PN532SEL")
+        { // client registration
+            $sql = "update productList set cardID = '" . test_input($_POST["cardID"]) . "' where productName = 'realtest'";
+            if ($conn->query($sql) === TRUE)
+            {
+                echo "Record updated successfully";
+            }
+            else {echo "Error: " . $sql . "<br>" . $conn->error;} 
+            $conn->close();
+        }
 
-
-    //     else
-    //     {
-    //         echo "Wrong Keyword provided.";
-    //         // echo $data["keyword"];
-    //     } // error
-    // }
+        else
+        {
+            echo "Wrong keyword provided.";
+        } // error
+    }
+    else
+    {
+        echo "No data posted with HTTP POST.";
+    }
     
 
-    // function test_input($data)
-    // {
-    //     $data = trim($data);
-    //     $data = stripslashes($data);
-    //     $data = htmlspecialchars($data);
-    //     return $data;
-    // }
-    //echo json_encode($data);
-    echo "testtest";
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 ?>
