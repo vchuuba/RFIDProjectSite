1f1b813a40699a3737933f1d63f075cc509042b5
Richardel Leang <richardel.leang@dawsoncollege.qc.ca>
2026-05-03 01:59:46 -0400

    cancel test

---
+++ itemCancel.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>item select page</title>
+    </head>
+    <body>
+        <?php
+            $servername = "localhost";
+            // Your Database name
+            $dbname = "RFID";
+            // Your Database user
+            $username = "testUser";
+            // Your Database user password
+            $password = "Shell111";
+
+                // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+                // Check connection
+            if ($conn->connect_error)
+            {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "SELECT id, cardID, productName, itemStatus FROM productList ORDER BY id DESC";
+            $result = $conn->query($sql);
+
+            echo
+            '
+                <table cellspacing="5" cellpadding="5">
+                    <tr> 
+                        <td>Locker #</td> 
+                        <td>Card ID</td>
+                        <td>Product Name</td>
+                        <td>Status</td>
+                    </tr>
+            ';
+            
+            while ($row = $result->fetch_assoc())
+            {
+                echo
+                '
+                    <tr>
+                        <td>' . $row["id"] . '</td>
+                        <td>' . $row["cardID"] . '</td>
+                        <td>' . $row["productName"] . '</td>
+                        <td>' . $row["itemStatus"] . '</td>
+                    </tr>
+                ';
+            }
+            echo '</table>';
+
+            $result = $conn->query($sql);
+            echo '<form method="POST" action="itemFree.php">';
+            echo '<p>Which to cancel?</p>';
+            while ($row = $result->fetch_assoc())
+            {
+                echo
+                '
+                    <input type="radio" id="' . $row["productName"] . '" name="choice" value="' . $row["productName"] . '">
+                    <label for="' . $row["productName"] . '">' . $row["productName"] . '</label><br>
+                ';
+            }
+            echo
+            '
+                <p>What is your username?</p>
+                    <label for="username">Username:</label><br>
+                    <input type="text" id="username" name="username" title="Latin alphabet only." minlength="1" maxlength="12" placeholder="Joesopher"><br>
+                    <input type="submit" value="Submit">
+                </form>
+            ';
+        ?>
+        <br>
+        <p><a href="index.html">Go back.</a> &lt;- Go back</p>
+    </body>
+</html>
---
+++ itemFree.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>item select page</title>
+    </head>
+    <body>
+        <?php
+            $servername = "localhost";
+            // Your Database name
+            $dbname = "RFID";
+            // Your Database user
+            $username = "testUser";
+            // Your Database user password
+            $password = "Shell111";
+
+                // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+                // Check connection
+            if ($conn->connect_error)
+            {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST")
+            {
+                $sql = "SELECT productName, username FROM productList WHERE username = '" . test_input($_POST["choice"]) . "'";
+                $result = $conn->query($sql);
+                while ($row = $result->fetch_assoc())
+                {
+                    $username = $row["username"];
+                }
+                if (test_input($_POST["username"]) == $username)
+                {
+                    $sql = "update productList set itemStatus = 'Available', client = NULL WHERE productName = '" . test_input($_POST["choice"]) . "'";
+                    $result = $conn->query($sql);
+                    echo mysqli_error($conn);
+                    echo '<p>Item cancelled successfully.</p>';
+                }
+                else
+                {
+                    echo '<p>Not permitted.</p>';
+                }
+                $conn->close();
+            }
+            else
+            {
+                echo '<p>No data posted with HTTP POST.</p>';
+            }
+            
+            function test_input($data)
+            {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                if ($data == "")
+                {
+                    return "!";
+                }
+                return $data;
+            }
+        ?>
+        <br>
+        <p><a href="index.html">Go back.</a> &lt;- Go back</p>
+    </body>
+</html>
