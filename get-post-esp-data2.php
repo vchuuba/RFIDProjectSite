35fc19931510cd8165bc569946394fc5b9e139b2
Richardel Leang <richardel.leang@dawsoncollege.qc.ca>
2026-05-03 23:44:26 -0400

    json reattempt

--- get-post-esp-data2.php
+++ get-post-esp-data2.php
@@ -42,6 +42,12 @@ $data = json_decode($raw, true);
 
         else if (($data["keyword"]) == "MFRC522INF")
         { // client authentication
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error)
+            {
+                die("Connection failed: " . $conn->connect_error);
+            }
             $sql = "SELECT * FROM productList";
             $result = $conn->query($sql);
             while ($row = $result->fetch_assoc())
